ce932fc6c77944fbe17d79f867f309bd5a62ac93
ElifRuveyha <ruveyhaozmen@gmail.com>
2023-08-10 15:53:31 +0200

    Draft AuthorizedUserPage, VaccineVerificationPage

---
+++ src/pages/VaccineVerificationPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default class Admin extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                VaccineVerificationPage
+            </div>
+        );
+    }
+}
