f4fb7f2b5b2830c47a7ac6da2467023d42b78b5a
OrdnasselaOttogim <alessandro.migotto@studenti.unitn.it>
2023-08-16 00:40:26 +0200

    vaccine verification page

--- src/pages/VaccineVerificationPage.js
+++ src/pages/VaccineVerificationPage.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CheckEligibility from '../functions/CheckEligibility'
+import CheckSingleVaccine from '../functions/CheckSingleVaccine'
+import Navbar from '../navbar'
 
 export default class Admin extends Component {
     constructor(props) {
@@ -9,7 +12,10 @@ export default class Admin extends Component {
     render() {
         return (
             <div>
-                VaccineVerificationPage
+                <Navbar account={this.props.account} />
+
+                <CheckEligibility account={this.props.account} eligibilityChecker={this.props.eligibilityChecker} entityList={this.props.entityList} />
+                <CheckSingleVaccine account={this.props.account} vaccineRegistry={this.props.vaccineRegistry}></CheckSingleVaccine>
             </div>
         );
     }
